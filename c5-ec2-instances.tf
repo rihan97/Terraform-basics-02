71c778098ee7677d96a2d99ecf091cd6d9e2ab37
rihan97 <rihan.alam97@gmail.com>
2023-01-16 22:09:54 +0000

    updated c5 file

--- c5-ec2-instances.tf
+++ c5-ec2-instances.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myec2" {
   instance_type = var.instance_type_list[0]   // this is for a list
   # instance_type = var.instance_type_map["dev"]   // this is for a map
   key_name = var.instance_keypair
-  user_data = file("/Users/rayhanalam/Documents/Training/Terraform/Terraform-basics-/User-data/app1-install.sh")
+  user_data = file("/Users/rayhanalam/Documents/Training/Terraform/Terraform-basics-02/User-data/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   count = 5  // either copy this whole block 5 times or use this count, this also has index variable 
   tags = {
